7a36bb66daed26ed542a3e6d8eb07623a100ac25
maykonchagas <maykon.chagas@ifsc.edu.br>
2021-08-17 07:10:19 -0300

    Iniciando o desenvolvimento do jogo Descubra o número

--- projects/guess-my-number/starter/script.js
+++ projects/guess-my-number/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+/*
+document.querySelector('.message');
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Você acertou!!!';
+
+document.querySelector('.number').textContent = 13;
+
+console.log(document.querySelector('.score'));
+//document.querySelector('.score').textContent = 10;
+//console.log((document.querySelector('.score').textContent = 10));
+*/
+console.log(document.querySelector('.guess').value);
+//document.querySelector('.guess').value = 10;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+});
