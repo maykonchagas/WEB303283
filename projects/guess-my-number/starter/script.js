1b5d098460ca26404685e272ac3d1b2eae5b5d8a
maykonchagas <maykon.chagas@ifsc.edu.br>
2021-09-14 10:30:50 -0300

    add: finalizado implementação do botão De novo!

--- projects/guess-my-number/starter/script.js
+++ projects/guess-my-number/starter/script.js
@@ -1,20 +1,63 @@
 'use strict';
-/*
-document.querySelector('.message');
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🎉 Você acertou!!!';
+// Sorteio do número que será realizado toda vez que iniciamos o jogo.
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
-document.querySelector('.number').textContent = 13;
+// Variável da pontuação, que será usada para decrementar o valor inicial
+let score = 20;
+let highscore = 0;
 
-console.log(document.querySelector('.score'));
-//document.querySelector('.score').textContent = 10;
-//console.log((document.querySelector('.score').textContent = 10));
-*/
-console.log(document.querySelector('.guess').value);
-//document.querySelector('.guess').value = 10;
+// Tirar no final do jogo
+//document.querySelector('.number').textContent = secretNumber;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+
+  if (!guess) {
+    displayMessage('⛔ Insira um número!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Você acertou!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 muito alto' : '📉 muito baixo');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 você perdeu!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Clique no botão De novo! Para voltar as mensagens no estado atual
+// e reiniciar o jogo sem perder o valor do highscore.
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+  console.log((secretNumber = Math.trunc(Math.random() * 20 + 1)));
+
+  document.querySelector('.message').textContent = '🔍 Adivinhe...';
+
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
